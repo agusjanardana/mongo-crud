f8cb10acba0f0ea4e630f76921eda1a09c441608
Agus Janardana <bjanardana@gmail.com>
2021-08-07 10:46:07 +0800

    salahnama

--- models/product.js
+++ models/product.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//create schema product disini
 var ProductSchema = new Schema({
-     kategori: String,
-     name: String,
-     price: Number,
+   kategori: String,
+   name: String,
+   price: Number,
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
